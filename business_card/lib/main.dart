6a8fc657f0877d2561169f5078befcf75b37aa66
Adriano Nascimento de Santana <adriano.santana@fastsolucoes.com.br>
2021-12-23 07:04:04 -0300

    Create business card

--- business_card/lib/main.dart
+++ business_card/lib/main.dart
@@ -13,34 +13,86 @@ class BusinessCardApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                margin: EdgeInsets.symmetric(vertical: 10),
+              const CircleAvatar(
+                radius: 58.0,
+                backgroundImage: AssetImage('assets/Adriano.jpeg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.yellow,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
+              const Text(
+                'Adriano Santana',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 23.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Container(
-                color: Colors.blue,
-                width: 100,
-                margin: EdgeInsets.symmetric(vertical: 10),
-              )
+                color: Colors.white,
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 20.0,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+55 81 995029086",
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                      size: 20.0,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "ans3@cin.ufpe.br",
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
